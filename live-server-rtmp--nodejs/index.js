c43bff6d8dbf401b4e31c8d9abd4d46fc7257db3
8bits-Nikk <nikhilmakwana028@gmail.com>
2023-05-15 19:20:57 +0530

    Added Server

---
+++ live-server-rtmp--nodejs/index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser')
+const NodeMediaServer = require('node-media-server');
+
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+app.use(session({
+  secret: "app-pathology-session-Secret",
+  resave: false,
+  saveUninitialized: false,
+  unset: 'destroy',
+  cookie: {
+    secure: false,
+    maxAge: null
+  }
+}));
+
+app.get('*', (req, res) => {
+  res.render('index');
+});
+
+
+const config = {
+  logType: 3,
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8888,
+    mediaroot: './server/media',
+    allow_origin: '*'
+  },
+  trans: {
+    ffmpeg: "C:\//ProgramData\//chocolatey\//bin\//ffmpeg.exe",
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+      }
+    ]
+  }
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
+console.log('started');
+module.exports = app.listen(5000);
+
